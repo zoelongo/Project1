15009edb7944710fc30d0fe6469db2cbd883ced4
April Long <april11dance@gmail.com>
2019-09-23 14:34:36 -0600

    we got maps

--- assets/javascript/ajax.js
+++ assets/javascript/ajax.js
@@ -1,5 +1,5 @@
 //api key// //Google Maps//
-// AIzaSyCcF1CUzKzcoWX96CTe1rgms1qhFd1vG7w
+//  AIzaSyDyp7bpK6RfNdcRxbAD33R3GDLqhKAfico&callback=initMap //
 
 //https key//
 // ""https://maps.googleapis.com/maps/api/js?key=AIzaSyCcF1CUzKzcoWX96CTe1rgms1qhFd1vG7w&callback=initMap"
@@ -8,25 +8,35 @@
 // var key1 = "200596500-c627d9f7783f081d34b8bd993d2abd94"
 // var queryUrl = "https://www.hikingproject.com/data/get-trails?lat=" + latitude + "&lon=" + longitude + "&maxDistance=10&key=" + key1 + ""
 
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    var uluru = { lat: -39.5501, lng: 105.7821 };
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 8, center: uluru });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({ position: uluru, map: map });
+}
 //Initialize Firebase//
 // $(document).ready(function () {
-    var firebaseConfig = {
-        apiKey: "AIzaSyBi-O17PsVdUQGHlfY3qhoiIBpnbuKWpM4",
-        authDomain: "test-7d555.firebaseapp.com",
-        databaseURL: "https://test-7d555.firebaseio.com",
-        projectId: "test-7d555",
-        storageBucket: "",
-        messagingSenderId: "98104578426",
-        appId: "1:98104578426:web:c2908835db8c009512c6c3"
-    };
+var firebaseConfig = {
+    apiKey: "AIzaSyBi-O17PsVdUQGHlfY3qhoiIBpnbuKWpM4",
+    authDomain: "test-7d555.firebaseapp.com",
+    databaseURL: "https://test-7d555.firebaseio.com",
+    projectId: "test-7d555",
+    storageBucket: "",
+    messagingSenderId: "98104578426",
+    appId: "1:98104578426:web:c2908835db8c009512c6c3"
+};
 
-    firebase.initializeApp(firebaseConfig);
-    var dataRef = firebase.database();
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: 0, lng: 0 },
-            zoom: 8
-        });
+firebase.initializeApp(firebaseConfig);
+var dataRef = firebase.database();
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 0, lng: 0 },
+        zoom: 8
+    });
     //buttons//
     $("button").on('click', function (event) {
         event.preventDefault()
@@ -34,26 +44,26 @@
         //declare variables
         console.log("clicked");
         //using lat/long on google map's api//
-        
+
 
         //AJAX request do we need? //to thr hiking api// create push pins
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            .then(function (response) {
-                var requests = response.data;
-                // var rating = requests[i].rating;
-                console.log(response)
-                makeajaxCall(requests);
-            });
+        // $.ajax({
+        //     url: queryURL,
+        //     method: "GET"
+        // })
+        //     .then(function (response) {
+        //         var requests = response.data;
+        //         // var rating = requests[i].rating;
+        //         console.log(response)
+        //         makeajaxCall(requests);
+        //     });
     })
     var map;
     function initialize() {
         var mapOptions = {
             zoom: 8,
-            center: { lat: -34.397, lng: 150.644 }
+            center: { lat: 44.2643, lng: 109.7870}
         };
         map = new google.maps.Map(document.getElementById('map'),
             mapOptions);
@@ -61,7 +71,7 @@
         var marker = new google.maps.Marker({
             // The below line is equivalent to writing:
             // position: new google.maps.LatLng(-34.397, 150.644)
-            position: { lat: -39.5501, lng: 105.7821 },
+            position: { lat: 44.2643, lng: 109.7870 },
             map: map
         });
 
@@ -80,15 +90,15 @@
     }
 
     google.maps.event.addDomListener(window, 'load', initialize);
-// })
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
-}
-
-
-
+    // })
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
     }
+
+
+
+}
